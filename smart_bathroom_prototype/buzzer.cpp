5d66cf4e2ac29973b78b32d936c8d3f946c981a6
Soryn <52570758+sorykkk@users.noreply.github.com>
2020-07-06 23:02:52 +0300

    Add files via upload

---
+++ smart_bathroom_prototype/buzzer.cpp
@@ -0,0 +1,22 @@
+#include "buzzer.h"
+#include <Arduino.h>
+
+void InitBuzzer()
+{
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void ActivateBuzzer()
+{
+  unsigned char i;
+
+  for(i = 0; i < BUZZER_FREQ; i++)
+  {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(10);
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(10);
+  }
+
+  //delay(500);
+}
